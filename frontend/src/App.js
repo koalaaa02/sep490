04db577fd15ef330e1baa603c4724857439fed6e
ManhNH-HE151021 <manhnhhe151021@fpt.edu.vn>
2025-03-19 00:42:41 +0700

    UI myAccountOrder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
-// react 
+// react
 import React from "react";
 // css
 import "./App.css";
-// browserrouter 
+// browserrouter
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // Components
-import Header from './Component/Header';
+import Header from "./Component/Header";
 import Footer from "./Component/Footer";
 // pages
 import Home from "./pages/Home";
@@ -37,7 +37,7 @@ const App = () => {
   return (
     <div>
       <Router>
-        <Header/>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           {/* Shop pages */}
@@ -53,10 +53,19 @@ const App = () => {
           {/* Accounts pages */}
           <Route path="/MyAccountOrder" element={<MyAccountOrder />} />
           <Route path="/MyAccountSetting" element={<MyAccountSetting />} />
-          <Route path="/MyAcconutNotification" element={<MyAcconutNotification />} />
-          <Route path="/MyAcconutPaymentMethod" element={<MyAcconutPaymentMethod />} />
+          <Route
+            path="/MyAcconutNotification"
+            element={<MyAcconutNotification />}
+          />
+          <Route
+            path="/MyAcconutPaymentMethod"
+            element={<MyAcconutPaymentMethod />}
+          />
           <Route path="/MyAccountAddress" element={<MyAccountAddress />} />
-          <Route path="/MyAccountForgetPassword" element={<MyAccountForgetPassword />} />
+          <Route
+            path="/MyAccountForgetPassword"
+            element={<MyAccountForgetPassword />}
+          />
           <Route path="/MyAccountSignIn" element={<MyAccountSignIn />} />
           <Route path="/MyAccountSignUp" element={<MyAccountSignUp />} />
           {/* About pages */}
@@ -65,7 +74,7 @@ const App = () => {
           <Route path="/Contact" element={<Contact />} />
           <Route path="/AboutUs" element={<AboutUs />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </Router>
     </div>
   );
---
+++ frontend/src/Component/Order/OrderDetail/OrderDetail.jsx
@@ -0,0 +1,76 @@
+import storeImg from "../../../images/stores-logo-1.svg";
+const OrderDetail = ({ order }) => {
+  console.log(order);
+
+  return (
+    <div className="d-flex p-2 border border-black  mt-2 mb-5 rounded shadow-sm">
+      <div className="order-info w-75 ">
+        <div className="text-black fw-bold fs-4 pb-2">
+          <span className="p-1">
+            <img
+              className="rounded-5"
+              src={storeImg}
+              width={50}
+              height={50}
+              alt=""
+            />
+          </span>
+          <span>{order.name}</span>
+        </div>
+        <div className="order-product">
+          <table class="table table-bordered">
+            <thead>
+              <tr className="text-center">
+                <th scope="col">Sẩn phẩm</th>
+                <th scope="col">Đơn giá</th>
+                <th scope="col">Số lượng</th>
+                <th scope="col">Thành tiền</th>
+              </tr>
+            </thead>
+            <tbody>
+              {order.products.map((p) => (
+                <tr className="">
+                  <th className="d-flex">
+                    <img
+                      width={200}
+                      height={150}
+                      className="object-fit-cover"
+                      src={p.src}
+                      alt=""
+                    />{" "}
+                    <span className="pl-1 fw-light ">{p.name}</span>
+                  </th>
+                  <td className="w-20 text-center">{p.price}</td>
+                  <td className=" w-20 text-center">
+                    <input
+                      // width={10}
+                      className="w-50 text-right"
+                      type="number"
+                      value={p.quantity}
+                    />
+                  </td>
+                  <td>{p.quantity * p.price}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div className="order-button w-25 p-2 text-center d-flex flex-column">
+        <div className="bg-primary w-75 rounded mx-auto p-3 text-white fw-bold fs-6 mt-5 ">
+          Xem chi tiết
+        </div>
+        <div className="my-auto">
+          <div className="bg-warning-subtle w-80 mx-auto mt-5 px-10 py-5 rounded border">
+            <p className="fs-6 fw-bold">Tổng tiền</p>
+            <div className="border border-dark px-5 py-1">3000303</div>
+          </div>
+          <div className="my-5 "> Chờ xét duyệt</div>
+          <div className="my-5">Huỷ yêu cầu</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OrderDetail;
