929c1a35d03965ebd467498ae156eb3cf4928792
NgHill <manhnhhe151021@fpt.edu.vn>
2025-03-26 23:48:16 +0700

    CheckOutForm

--- frontend/src/pages/Accounts/MyDebt/MyDebt.jsx
+++ frontend/src/pages/Accounts/MyDebt/MyDebt.jsx
@@ -6,10 +6,19 @@ import styles from "./MyDebt.module.css";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import classNames from "classnames";
 import CheckoutForm from "../../../Component/MyDebt/CheckoutForm";
-const DebtOrder = (item) => {
-  const items = item?.item;
+import { BASE_URL } from "../../../Utils/config";
+const DebtOrder = ({ item, activeShop, setActiveShop, orders }) => {
   return (
-    <div className={styles.debtOrderCard}>
+    <div
+      className={styles.debtOrderCard}
+      onClick={() => {
+        if (item?.id === activeShop) {
+          setActiveShop(null);
+        } else {
+          setActiveShop(item?.id);
+        }
+      }}
+    >
       <div
         className={classNames(
           "d-flex align-items-center justify-content-between px-2"
@@ -22,78 +31,142 @@ const DebtOrder = (item) => {
           )}
         >
           <AiOutlineShoppingCart />
-          {items?.shopName}
+          {item?.shopName}
         </div>
-        <input type="checkbox" />
+        <input type="checkbox" checked={item?.id === activeShop} />
       </div>
-      {items?.orders?.map((o, index) => (
-        <div className={styles.itemCard} key={index}>
-          <img src={o?.itemImage} alt="" />
-          <div className={styles.infoCard}>
-            <div className="d-flex flex-column fw-bold">
-              <span>{o?.name}</span>
-              <span>50kg/1 bao</span>
+      {item?.id === activeShop && (
+        <>
+          {orders?.map((o, index) => (
+            <div className={styles.itemCard} key={index}>
+              <img src={o?.itemImage} alt="" />
+              <div className={styles.infoCard}>
+                <div className="d-flex flex-column fw-bold">
+                  <span>{o?.orderName}</span>
+                  {/* <span>50kg/1 bao</span> */}
+                </div>
+                <div className={classNames(styles.middlePart)}>
+                  <span>
+                    {Intl.NumberFormat("vi-VN", {
+                      style: "currency",
+                      currency: "VND",
+                    }).format(o?.price)}
+                    /1 lần
+                  </span>
+                  <input
+                    className="inputNumber"
+                    min="1"
+                    disabled
+                    value={o?.amount}
+                    defaultValue="1"
+                  />
+                </div>
+                <span>
+                  {Intl.NumberFormat("vi-VN", {
+                    style: "currency",
+                    currency: "VND",
+                  }).format(o?.amount * o?.price)}
+                </span>
+              </div>
             </div>
-            <div className={classNames(styles.middlePart)}>
-              <span>1.500.000 đ/1 lần</span>
-              <input className="inputNumber" min="1" defaultValue="1" />
-            </div>
-            <span>1.500.000 vnđ</span>
+          ))}
+          <div className="d-flex justify-content-between align-items-center">
+            <span className="fw-bold">Tổng tiền nợ của đơn hàng</span>
+            <span>
+              {Intl.NumberFormat("vi-VN", {
+                style: "currency",
+                currency: "VND",
+              }).format(
+                orders?.reduce((acc, index) => {
+                  return acc + index?.price * index?.amount;
+                }, 0)
+              )}
+            </span>
           </div>
-        </div>
-      ))}
-      <div className="d-flex justify-content-between align-items-center">
-        <span className="fw-bold">Tổng tiền nợ của đơn hàng</span>
-        <span>300.000 vnđ</span>
-      </div>
+        </>
+      )}
     </div>
   );
 };
 const MyDebt = () => {
+  const token = localStorage.getItem("access_token");
   const [loaderStatus, setLoaderStatus] = useState(true);
   const [showModal, setShowModal] = useState(false);
-  const items = [
-    {
-      shopName: "Shop 1",
-      orders: [
-        {
-          itemImage:
-            "https://c4.wallpaperflare.com/wallpaper/987/319/586/windows-11-dark-theme-silk-hd-wallpaper-preview.jpg",
-          name: "Xi măng",
-        },
-        {
-          itemImage:
-            "https://c4.wallpaperflare.com/wallpaper/987/319/586/windows-11-dark-theme-silk-hd-wallpaper-preview.jpg",
-          name: "Xi măng",
-        },
-        {
-          itemImage:
-            "https://c4.wallpaperflare.com/wallpaper/987/319/586/windows-11-dark-theme-silk-hd-wallpaper-preview.jpg",
-          name: "Xi măng",
-        },
-      ],
-    },
-    {
-      shopName: "Shop 2",
-      orders: [
-        {
-          itemImage:
-            "https://c4.wallpaperflare.com/wallpaper/987/319/586/windows-11-dark-theme-silk-hd-wallpaper-preview.jpg",
-          name: "Xi măng",
-        },
-        {
-          itemImage:
-            "https://c4.wallpaperflare.com/wallpaper/987/319/586/windows-11-dark-theme-silk-hd-wallpaper-preview.jpg",
-          name: "Xi măng",
-        },
-      ],
-    },
-  ];
+  const [activeShop, setActiveShop] = useState(null);
+  const [orders, setOrders] = useState([]);
   useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `${BASE_URL}/api/dealer/GetInvoicesByShopId/1`,
+          {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${token}`,
+              "Content-Type": "application/json",
+            },
+            credentials: "include",
+          }
+        );
+        const result = await response.json();
+        console.log(result);
+      } catch (error) {
+        console.error("Lỗi khi fetch dữ liệu:", error);
+      }
+    };
+
+    fetchData();
     setTimeout(() => {
       setLoaderStatus(false);
     }, 1500);
   }, []);
+
+  const shops = [
+    {
+      id: 1,
+      shopName: "shop1",
+    },
+    {
+      id: 2,
+      shopName: "shop2",
+    },
+    {
+      id: 3,
+      shopName: "shopDunk",
+    },
+  ];
+
+  const activeShopOrders = [
+    {
+      orderName: "Xi măng 50kg/1 bao",
+      amount: 2,
+      price: 1500000,
+      itemImage:
+        "https://i.pcmag.com/imagery/reviews/00xBy0JjVybodfIwWxeGCkZ-1.fit_lim.size_1050x591.v1679417407.jpg",
+    },
+    {
+      orderName: "Cát xây dựng 1m3",
+      amount: 5,
+      price: 800000,
+      itemImage:
+        "https://cdn.mos.cms.futurecdn.net/uPALkW3UzvgE6FhMy3nCzD-1200-80.png",
+    },
+    {
+      orderName: "Gạch xây dựng loại 10x20",
+      amount: 50,
+      price: 5000,
+      itemImage:
+        "https://i.pcmag.com/imagery/reviews/00xBy0JjVybodfIwWxeGCkZ-1.fit_lim.size_1050x591.v1679417407.jpg",
+    },
+    {
+      orderName: "Đá 1x2 1m3",
+      amount: 3,
+      price: 900000,
+      itemImage:
+        "https://cdn.mos.cms.futurecdn.net/uPALkW3UzvgE6FhMy3nCzD-1200-80.png",
+    },
+  ];
+
   return (
     <div>
       <>
@@ -157,8 +230,14 @@ const MyDebt = () => {
                                   Các đơn hàng đang còn nợ
                                 </h5>
                                 <div className={styles.itemsList}>
-                                  {items?.map((i, index) => (
-                                    <DebtOrder item={i} key={index} />
+                                  {shops?.map((i, index) => (
+                                    <DebtOrder
+                                      item={i}
+                                      key={i?.id}
+                                      activeShop={activeShop}
+                                      orders={activeShopOrders}
+                                      setActiveShop={setActiveShop}
+                                    />
                                   ))}
                                 </div>
                               </div>
@@ -176,15 +255,30 @@ const MyDebt = () => {
                                 <p>Số tiền cần thanh toàn</p>
                                 <p
                                   className={classNames(
-                                    "text-danger",
+                                    !activeShop && "text-danger",
                                     styles.totalPrice
                                   )}
                                 >
-                                  300.000 vnđ
+                                  {activeShop
+                                    ? Intl.NumberFormat("vi-VN", {
+                                        style: "currency",
+                                        currency: "VND",
+                                      }).format(
+                                        activeShopOrders?.reduce(
+                                          (acc, index) => {
+                                            return (
+                                              acc + index?.price * index?.amount
+                                            );
+                                          },
+                                          0
+                                        )
+                                      )
+                                    : "Please choose a shop "}
                                 </p>
                               </div>
                               <div className="d-flex justify-content-center">
                                 <button
+                                  disabled={!activeShopOrders}
                                   className={classNames(
                                     "bg-black text-white",
                                     styles.cashOutButton
